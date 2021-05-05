a5189ddc391f5c88dd786d8d07c13957d9fe4092
Bianca Brown <biancakbrown58@gmail.com>
2021-05-05 13:08:14 -0400

    reverse string controller method working

---
+++ Controllers/ReverseStringController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExplosionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReverseStringController : ControllerBase
+    {
+        [HttpGet("{str}")]
+        public string Solution(string str)
+        {
+            // var sentence = "this is the string";
+            return new string(str.ToArray().Reverse().ToArray());
+        }
+
+    }
+}
+// public static string Solution(string str) 
+//   {
+//      return new string(str.ToArray().Reverse().ToArray());
+//   }
